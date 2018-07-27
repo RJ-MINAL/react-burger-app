0d3fb3423119c93c2677be0adf81a0654173bab2
RJ_Minal <rj.minera@gmail.com>
2018-07-27 15:24:07 -0600

    add: Toolbar & NavigationItems components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
-import classes from './Layout.css'
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
-const layout = (props) => (
-    <React.Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </React.Fragment>
+const layout = props => (
+  <React.Fragment>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
+  </React.Fragment>
 );
 
 export default layout;
